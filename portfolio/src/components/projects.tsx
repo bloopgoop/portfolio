a3e11fa419a1d81a45e838efc05ba3b7f9e9a5ca
bloopgoop <zhu.kevin12@gmail.com>
2024-02-14 02:08:06 -0500

    Add project images 1

--- portfolio/src/components/projects.tsx
+++ portfolio/src/components/projects.tsx
@@ -1,24 +1,36 @@
+"use client";
+import { cn } from "@/utils/cn";
+import staticEssentials from "../assets/essentialsEstate2-13-2024.png";
+import staticHuff from "../assets/staticHuff.png";
+import animatedHuff from "../assets/animatedHuff.gif";
+import staticTraffic from "../assets/staticTraffic.png";
+import animatedTraffic from "../assets/animatedTraffic.gif";
+import imageNotFound from "../assets/image-not-found.jpg";
 import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
 import {
   IconArrowWaveRightUp,
   IconBoxAlignRightFilled,
   IconBoxAlignTopLeft,
-  IconClipboardCopy,
-  IconFileBroken,
-  IconSignature,
+  IconBuildingEstate,
+  IconBinaryTree,
+  IconTrafficCone,
   IconTableColumn,
 } from "@tabler/icons-react";
 
 export default function Portfolio() {
   return (
     <div id="projects">
+      <h1 className="text-base sm:text-xl md:text-3xl lg:text-5xl font-bold text-center mt-20 mb-10">
+        Projects
+      </h1>
       <BentoGrid className="max-w-7xl mx-auto">
         {items.map((item, i) => (
           <BentoGridItem
             key={i}
             title={item.title}
             description={item.description}
-            header={item.header}
+            gif={item.gif}
+            image={item.image}
             icon={item.icon}
             className={i === 0 || i === 6 ? "md:col-span-2" : ""}
           />
@@ -28,51 +40,81 @@ export default function Portfolio() {
   );
 }
 
-const Skeleton = () => (
-  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+const Skeleton = ({
+  image,
+  className,
+}: {
+  image: string;
+  className?: string;
+}) => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100">
+    <img
+      src={image}
+      alt="Skeleton"
+      className={cn("w-full h-full object-cover rounded-xl", className)}
+    />
+  </div>
 );
+
+const AnimatedSkeleton = ({
+  image,
+}: {
+  image: string;
+  className?: string;
+}) => (
+  Skeleton({ image, className:"object-contain" })
+);
+
 const items = [
   {
-    title: "The Dawn of Innovation",
-    description: "Explore the birth of groundbreaking ideas and inventions.",
-    header: <Skeleton />,
-    icon: <IconClipboardCopy className="h-4 w-4 text-neutral-500" />,
+    title: "Essentials Estate",
+    description:
+      "Full stack web application designed to streamline tenant and owner rental interactions. It is built keeping in mind certain requirements and restrictions given by a Systems Analysis and Design class.",
+    gif: (
+      <AnimatedSkeleton
+        image="https://github.com/bloopgoop/property-management/assets/104113781/5b0fe21f-a4dd-4696-9a3b-def43e4d13b9"
+      />
+    ),
+    image: <Skeleton image={staticEssentials} />,
+    icon: <IconBuildingEstate className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Digital Revolution",
-    description: "Dive into the transformative power of technology.",
-    header: <Skeleton />,
-    icon: <IconFileBroken className="h-4 w-4 text-neutral-500" />,
+    title: "Huffman Algorithm",
+    description: "Implementation of compression technique used to reduce the size of data files. Helps with storage and transferring files.",
+    gif: <AnimatedSkeleton image={animatedHuff}/>,
+    image: <Skeleton image={staticHuff} />,
+    icon: <IconBinaryTree className="h-4 w-4 text-neutral-500" />,
   },
   {
-    title: "The Art of Design",
-    description: "Discover the beauty of thoughtful and functional design.",
-    header: <Skeleton />,
-    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+    title: "Traffic",
+    description: "Used convolutional neural networks to distinguish between traffic signs.",
+    gif: <AnimatedSkeleton image={animatedTraffic}/>,
+    image: <Skeleton image={staticTraffic} />,
+    icon: <IconTrafficCone className="h-4 w-4 text-neutral-500" />,
   },
   {
     title: "The Power of Communication",
     description:
       "Understand the impact of effective communication in our lives.",
-    header: <Skeleton />,
+    image: <Skeleton image={imageNotFound}/>,
     icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
   },
   {
     title: "The Pursuit of Knowledge",
     description: "Join the quest for understanding and enlightenment.",
-    header: <Skeleton />,
+    image: <Skeleton image={imageNotFound}/>,
     icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
   },
   {
     title: "The Joy of Creation",
     description: "Experience the thrill of bringing ideas to life.",
-    header: <Skeleton />,
+    image: <Skeleton image={imageNotFound}/>,
     icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
   },
   {
     title: "The Spirit of Adventure",
     description: "Embark on exciting journeys and thrilling discoveries.",
-    header: <Skeleton />,
+    image: <Skeleton image={imageNotFound}/>,
     icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
   },
 ];
--- portfolio/src/components/ui/bento-grid.tsx
+++ portfolio/src/components/ui/bento-grid.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/utils/cn";
+import { useState } from "react";
 
 export const BentoGrid = ({
   className,
@@ -10,7 +11,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid md:auto-rows-[30rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
         className
       )}
     >
@@ -23,23 +24,29 @@ export const BentoGridItem = ({
   className,
   title,
   description,
-  header,
+  gif,
+  image,
   icon,
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
-  header?: React.ReactNode;
+  gif?: React.ReactNode;
+  image?: React.ReactNode;
   icon?: React.ReactNode;
 }) => {
+  const [visible, setVisible] = useState(false); // initiate it at false
+
   return (
     <div
       className={cn(
         "row-span-1 rounded-xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
+      onMouseEnter={() => setVisible(true)}
+      onMouseLeave={() => setVisible(false)}
     >
-      {header}
+      {(visible && gif) ? gif : image}
       <div className="group-hover/bento:translate-x-2 transition duration-200">
         {icon}
         <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
